4be4ece5422d38dc5037d95ea3222916636103e4
viraj8206 <109304745+viraj8206@users.noreply.github.com>
2022-07-18 19:00:48 +0530

    himalya refrigration

---
+++ about.js
@@ -0,0 +1,7 @@
+$('.Manu').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+})
